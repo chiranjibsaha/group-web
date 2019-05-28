bec39b3c642091f4dab8f043d2730e08035bcb99
stochastic-geometry <chiranjib92@chiranjibs-mbp.home>
2019-05-27 21:22:45 -0500

    updates

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Poisson Cluster Process: Theories and Application in Wireless Network Models",
         "excerpt":"This page maintains our progress on the Poisson cluster process (PCP)-based modeling and analysis of cellular networks. A brief background Stochastic geometry (SG) has emerged as a powerful tool for modelling and analysis of wireless cellular networks. Using the distributions of random point patterns, which are the key offerings of...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/PCP/",
+        "url": "/PCP/",
         "teaser":null}]
